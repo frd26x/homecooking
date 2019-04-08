57b6ec6f5e1e3dd3eecedc5d27ae0abe2f97a9cd
federicoCassettai <f.cassettai@gmail.com>
2019-04-08 18:25:57 +0200

    App turned in functional component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,45 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Link, NavLink, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import api from "../api";
-import logo from "../logo.svg";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  handleLogoutClick(e) {
+export default function App() {
+  const handleLogoutClick = e => {
     api.logout();
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-          {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
-          {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
-          {api.isLoggedIn() && (
-            <Link to="/" onClick={e => this.handleLogoutClick(e)}>
-              Logout
-            </Link>
-          )}
-        </header>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route render={() => <h2>404</h2>} />
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
+        {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
+        {api.isLoggedIn() && (
+          <Link to="/" onClick={e => handleLogoutClick(e)}>
+            Logout
+          </Link>
+        )}
+      </header>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route render={() => <h2>404</h2>} />
+      </Switch>
+    </div>
+  );
 }
